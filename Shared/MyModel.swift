4ca9e926f985fa167247c69399e767004c94bbd2
MichaelStreet <streetymike@gmail.com>
2022-05-24 13:24:34 +1000

    Add MyModel to Project Use present tense

---
+++ Shared/MyModel.swift
@@ -0,0 +1,18 @@
+//
+//  MyModel.swift
+//  LearningGH
+//
+//  Created by Michael Street on 24/5/2022.
+//
+
+import Foundation
+
+
+
+struct MYModel {
+    
+    var name:String?
+    
+    
+    
+}
