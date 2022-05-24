7b5d274daba51f1d71b8f00c361040db7de9053c
streetymike <streetymike@gmail.com>
2022-05-24 13:30:48 +1000

    Changed text
    add diff text

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, codecrew!")
+        Text("Hello, codecrew you rock!")
             .padding()
     }
 }
